9ed2d228a25ab761a7965aa49f58a4555ea301fb
MartinBriday <briday.martin@gmail.com>
2023-03-13 12:39:15 +0100

    Add repository interfaces

---
+++ src/main/java/fr/acme/CRM/repository/ProduitRepository.java
@@ -0,0 +1,9 @@
+package fr.acme.CRM.repository;
+
+import fr.acme.CRM.model.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Integer> {
+}
