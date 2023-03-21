2d065ca662d8793e966250ba09a19e8fb65e2dcf
Ferdaous Aloui <aloui.ferdaous23@gmail.com>
2023-03-21 12:00:18 +0100

    last version of interface

--- UI/js/commande.js
+++ UI/js/commande.js
@@ -195,6 +195,11 @@ class Commande {
     }
 }
 
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+  button.innerHTML = `Nombre de clics : ${event.detail}`;
+});
 
 
 $(document).ready(() => {
